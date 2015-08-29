4fd7196ef1ddf04f7de21ffe7257802c2871f93f
efuste <erik.fuste@pxl83.com>
2015-08-29 13:34:31 -0400

    Initial Commit

---
+++ Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Erik Fuste on 8/29/15.
+//  Copyright (c) 2015 Erik Fuste. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Erik Fuste on 8/29/15.
+//  Copyright (c) 2015 Erik Fuste. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
